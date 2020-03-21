34784a945294dde2939b47612506bc67dcc1e22b
pieroashady <aldipiero10@gmail.com>
2020-03-21 12:55:43 +0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ListClub from './components/ListClub';
 import FootballAreas from './components/FootballAreas';
 import ClubProfile from './components/ClubProfile';
+import Navigation from './components/Navbar';
 
 class Index extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <div>
+          <Navigation />
           <Switch>
             <Route exact path='/areas/:id' component={ListClub} />
-            <Route path='/areas' component={FootballAreas} />
+            <Route exact path='/' component={FootballAreas} />
             <Route exact path='/club-profile/:id' component={ClubProfile} />
           </Switch>
         </div>
